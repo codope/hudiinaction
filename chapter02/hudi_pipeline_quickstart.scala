1aed27915c731532c2a52eb4723ef5f43cbedc32
Sagar Sumit <sagarsumit09@gmail.com>
2025-06-28 20:36:26 +0530

    Update script; no custom column
    Signed-off-by: Sagar Sumit <sagarsumit09@gmail.com>

--- chapter02/hudi_pipeline_quickstart.scala
+++ chapter02/hudi_pipeline_quickstart.scala
@@ -12,21 +12,17 @@ val inputPath = "/Users/username/path/to/trips_0"
 val basePath  = "/tmp/trips_table"
 
 // read raw CSV (tab-separated)
-val raw = spark.read.format("csv").
+val df = spark.read.format("csv").
   option("header", "true").
   option("sep",    "\t").
   load(inputPath).
   toDF()
 
-// derive an event-time column for precombine and make fare a double type
-val df = raw.withColumn("pickup_ts", unix_timestamp($"pickup_datetime").cast("long")).
-  withColumn("fare_amount", $"fare_amount".cast(DoubleType))
-
 // First create a CoW table
 df.write.format("hudi")
   .option("hoodie.datasource.write.recordkey.field",     "trip_id")
   .option("hoodie.datasource.write.partitionpath.field", "vendor_id")
-  .option("hoodie.datasource.write.precombine.field",    "pickup_ts")
+  .option("hoodie.datasource.write.precombine.field",    "pickup_datetime")
   .option("hoodie.datasource.write.hive_style_partitioning", "true")
   .option("hoodie.table.name",                     "nyc_taxi_trips")
   .mode("Overwrite")
@@ -35,38 +31,43 @@ df.write.format("hudi")
 // Smoke-test a read
 val snapshotDf = spark.read.format("hudi").load(basePath)
 snapshotDf.count() // 1000660 rows
-snapshotDf.select("trip_id", "vendor_id", "pickup_ts", "fare_amount").filter("vendor_id = '1'").limit(2).show()
-//  +----------+---------+----------+-----------+
-//  |   trip_id|vendor_id| pickup_ts|fare_amount|
-//  +----------+---------+----------+-----------+
-//  |1207977523|        1|1439183194|       16.0|
-//  |1208792114|        1|1439254067|        9.5|
-//  +----------+---------+----------+-----------+
+snapshotDf.select("trip_id", "vendor_id", "pickup_datetime", "fare_amount").filter("vendor_id = '1'").limit(2).show()
+// +----------+---------+-------------------+-----------+
+// |   trip_id|vendor_id|    pickup_datetime|fare_amount|
+// +----------+---------+-------------------+-----------+
+// |1207977523|        1|2015-08-10 10:36:34|         16|
+// |1208792114|        1|2015-08-11 06:17:47|        9.5|
+// +----------+---------+-------------------+-----------+
 
 // Build an update dataframe
 val toUpsert = df.filter($"trip_id" === "1207977523").
-  withColumn("fare_amount", $"fare_amount" * 1.2).
-  withColumn("pickup_ts",   $"pickup_ts" * 2)
+  withColumn("fare_amount", $"fare_amount".cast(DoubleType) * 1.2).
+  withColumn("pickup_datetime",   from_unixtime(unix_timestamp($"pickup_datetime") + 3600))
 
 // Update
 toUpsert.write.format("hudi")
   .option("hoodie.datasource.write.operation",  "upsert")
-  .mode("Append")              // don’t overwrite the table
+  .mode("Append")
   .save(basePath)
 
 // read the record
 val snapshotDf = spark.read.format("hudi").load(basePath)
-snapshotDf.select("trip_id", "vendor_id", "pickup_ts", "fare_amount").filter($"trip_id" === "1207977523").show()
-//  +----------+---------+----------+-----------+
-//  |   trip_id|vendor_id| pickup_ts|fare_amount|
-//  +----------+---------+----------+-----------+
-//  |1207977523|        1|2878366388|       19.2|
-//  +----------+---------+----------+-----------+
+snapshotDf.select("trip_id", "vendor_id", "pickup_datetime", "fare_amount").filter($"trip_id" === "1207977523").show()
+// +----------+---------+-------------------+-----------+
+// |   trip_id|vendor_id|    pickup_datetime|fare_amount|
+// +----------+---------+-------------------+-----------+
+// |1207977523|        1|2015-08-10 11:36:34|       19.2|
+// +----------+---------+-------------------+-----------+
 
 // Issue a delete
 val toDelete = df.filter($"trip_distance".cast("double") < 0.1)
 toDelete.count() // 7900 rows
-toDelete.select("trip_id", "vendor_id", "pickup_ts", "trip_distance").filter("vendor_id = '1'").limit(1).show()
+toDelete.select("trip_id", "vendor_id", "pickup_datetime", "trip_distance").filter("vendor_id = '1'").limit(1).show()
+// +----------+---------+-------------------+-------------+
+// |   trip_id|vendor_id|    pickup_datetime|trip_distance|
+// +----------+---------+-------------------+-------------+
+// |1200001601|        1|2015-07-16 23:38:37|            0|
+// +----------+---------+-------------------+-------------+
 
 toDelete.write.format("hudi")
   .option("hoodie.datasource.write.operation", "delete") // key line
@@ -75,7 +76,11 @@ toDelete.write.format("hudi")
 
 val snapshotDf = spark.read.format("hudi").load(basePath)
 snapshotDf.count() // 1000660 - 7900 = 992760 rows
-snapshotDf.select("trip_id", "vendor_id", "pickup_ts", "fare_amount").filter($"trip_id" === "1200001601").show()
+snapshotDf.select("trip_id", "vendor_id", "pickup_datetime", "fare_amount").filter($"trip_id" === "1200001601").show()
+// +-------+---------+---------------+-----------+
+// |trip_id|vendor_id|pickup_datetime|fare_amount|
+// +-------+---------+---------------+-----------+
+// +-------+---------+---------------+-----------+
 
 import org.apache.hudi.common.table.HoodieTableMetaClient
 import org.apache.hudi.storage.hadoop.HadoopStorageConfiguration
@@ -101,7 +106,7 @@ val deleteCommit = commits.get(2)
 val roDf = spark.read.format("hudi").
   option("hoodie.datasource.query.type", "read_optimized").
   load(basePath)
-roDf.count()  // same as snapshot on CoW but will be different for MoR
+roDf.count()  // 992760 rows same as snapshot on CoW but will be different for MoR
 
 // Incremental query
 val incrDf = spark.read.format("hudi").
@@ -112,31 +117,31 @@ val incrDf = spark.read.format("hudi").
 
 // Note that begin instant time is inclusive,
 // while end instant time is exclusive.
-incrDf.select("trip_id", "vendor_id", "pickup_ts", "fare_amount").show(false)
-//  +----------+---------+----------+-----------+
-//  |trip_id   |vendor_id|pickup_ts |fare_amount|
-//  +----------+---------+----------+-----------+
-//  |1207977523|1        |2878366388|19.2       |
-//  +----------+---------+----------+-----------+
+incrDf.select("trip_id", "vendor_id", "pickup_datetime", "fare_amount").show(false)
+// +----------+---------+-------------------+-----------+
+// |trip_id   |vendor_id|pickup_datetime    |fare_amount|
+// +----------+---------+-------------------+-----------+
+// |1207977523|1        |2015-08-10 11:36:34|19.2       |
+// +----------+---------+-------------------+-----------+
 
 // Time travel query
 val ttDf = spark.read.format("hudi").
   option("as.of.instant", upsertCommit).   // point-in-time
   load("/tmp/trips_table")
-ttDf.count()                // back to original row count
-ttDf.select("trip_id", "vendor_id", "pickup_ts", "fare_amount").filter($"trip_id" === "1200001601").show()
-//  +----------+---------+----------+-----------+
-//  |   trip_id|vendor_id| pickup_ts|fare_amount|
-//  +----------+---------+----------+-----------+
-//  |1200001601|        1|1437070117|        3.0|
-//  +----------+---------+----------+-----------+
+ttDf.count()                // back to original row count 1000660
+ttDf.select("trip_id", "vendor_id", "pickup_datetime", "fare_amount").filter($"trip_id" === "1200001601").show()
+// +----------+---------+-------------------+-----------+
+// |   trip_id|vendor_id|    pickup_datetime|fare_amount|
+// +----------+---------+-------------------+-----------+
+// |1200001601|        1|2015-07-16 23:38:37|          3|
+// +----------+---------+-------------------+-----------+
 
 // Create Merge-on-Read Table
 val morBasePath = "/tmp/trips_table_mor"
 df.write.format("hudi").
   option("hoodie.datasource.write.recordkey.field",     "trip_id").
   option("hoodie.datasource.write.partitionpath.field", "vendor_id").
-  option("hoodie.datasource.write.precombine.field",    "pickup_ts").
+  option("hoodie.datasource.write.precombine.field",    "pickup_datetime").
   option("hoodie.datasource.write.hive_style_partitioning", "true").
   // all other options remain same as CoW, just set the table type
   option("hoodie.datasource.write.table.type",     "MERGE_ON_READ").
@@ -150,6 +155,23 @@ toUpsert.write.format("hudi")
   .option("hoodie.datasource.write.operation", "upsert")
   .mode("Append")
   .save(morBasePath)
+
+// Delete MoR table w/o compaction
+toDelete.write.format("hudi")
+  .option("hoodie.datasource.write.table.type", "MERGE_ON_READ")
+  .option("hoodie.datasource.write.operation", "delete")
+  .mode("Append")
+  .save(morBasePath)
+
+// Snapshot query on MoR
+val snapshotDf = spark.read.format("hudi").load(morBasePath)
+snapshotDf.count() // 992760 rows
+
+// Read-optimized query
+val roDf = spark.read.format("hudi").
+  option("hoodie.datasource.query.type", "read_optimized").
+  load(morBasePath)
+roDf.count() // 1000660 rows
 
 // Update MoR table with compaction
 toUpsert.write.format("hudi")
@@ -182,4 +204,3 @@ df.limit(0).write.format("hudi")
   .option("hoodie.clean.automatic",               "true")
   .mode("Append")
   .save(morBasePath)
-
